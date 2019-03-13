9cd0230c289c0b308711c59f5908f4ea45744975
Mei'lani <meyrehay@gmail.com>
2019-03-13 16:18:18 -0700

    Completed with live data

--- app/src/main/java/edu/cascadia/mobile/apps/passwordcheck/MainActivity.java
+++ app/src/main/java/edu/cascadia/mobile/apps/passwordcheck/MainActivity.java
@@ -2,6 +2,7 @@ package edu.cascadia.mobile.apps.passwordcheck;
 
 import android.arch.lifecycle.ViewModel;
 import android.arch.lifecycle.ViewModelProvider;
+import android.arch.lifecycle.ViewModelProviders;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,20 +11,16 @@ import edu.cascadia.mobile.apps.passwordcheck.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    //TODO: create a private ViewModel instance variable
-
+    private PasswordViewModel pvm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //Comment out the setContentView - we'll use a binding instead
         setContentView(R.layout.activity_main);
 
-        //TODO: Create a new binding using DataBindingUtil
-
-        //TODO: Create a new PasswordViewModel object
-
-        //TODO: Set the binding's ViewModel to your new PasswordViewModel
-
-
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        pvm = ViewModelProviders.of(this).get(PasswordViewModel.class);
+        binding.setViewModel(pvm);
+        binding.setLifecycleOwner(this);
     }
 }
--- app/src/main/java/edu/cascadia/mobile/apps/passwordcheck/PasswordViewModel.java
+++ app/src/main/java/edu/cascadia/mobile/apps/passwordcheck/PasswordViewModel.java
@@ -1,36 +1,46 @@
 package edu.cascadia.mobile.apps.passwordcheck;
 // Adapted from https://www.bignerdranch.com/blog/two-way-data-binding-on-android-observing-your-view-with-xml/
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.ViewModel;
 import android.databinding.BaseObservable;
 import android.databinding.Bindable;
 
-public class PasswordViewModel extends BaseObservable {
-    private String password;
+public class PasswordViewModel extends ViewModel {
+    private MutableLiveData<String> password;
+    private MutableLiveData<String> passwordQuality;
 
-    @Bindable
+    public PasswordViewModel(){
+        this.password = new MutableLiveData<>();
+        this.passwordQuality = new MutableLiveData<>();
+    }
     public String getPassword() {
-        return password;
+        return password.getValue();
     }
 
-    @Bindable
     public String getPasswordQuality() {
-        if (password == null || password.isEmpty()) {
+        if (password == null || password.getValue().isEmpty()) {
             return "Enter a password";
-        } else if (password.equals("password")) {
+        } else if (password.getValue().equals("password")) {
             return "Very bad";
-        } else if (password.length() < 6) {
+        } else if (password.getValue().length() < 6) {
             return "Short";
         } else {
             return "Okay";
         }
     }
 
-    public void setPassword(String password) {
-        if(this.password != password) {
-            this.password = password;
-            notifyPropertyChanged(BR.passwordQuality);
-            notifyPropertyChanged(BR.password);
+    public void setPassword(String pass) {
+        if(this.password.getValue() != pass) {
+            this.password.setValue(pass);
+          //  notifyPropertyChanged(BR.passwordQuality);
+          //  notifyPropertyChanged(BR.password);
         }
+
     }
 
 
+    public void onPasswordTextChanged(CharSequence cs){
+        setPassword(cs.toString());
+        getPasswordQuality();
+    }
 }
