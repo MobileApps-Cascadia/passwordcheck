e2bb745b3b5a77db77f177d9a82e6205c48d096b
Josh Martinez <joshuaaaronmartinez@gmail.com>
2020-02-20 17:01:53 -0800

    Migrated to androidx Added two-way-binding

--- app/src/androidTest/java/edu/cascadia/mobile/apps/passwordcheck/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/edu/cascadia/mobile/apps/passwordcheck/ExampleInstrumentedTest.java
@@ -1,6 +1,8 @@
 package edu.cascadia.mobile.apps.passwordcheck;
 
+
 import android.content.Context;
+
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
@@ -19,7 +21,7 @@ public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
         assertEquals("edu.cascadia.mobile.apps.passwordcheck", appContext.getPackageName());
     }
--- app/src/main/java/edu/cascadia/mobile/apps/passwordcheck/MainActivity.java
+++ app/src/main/java/edu/cascadia/mobile/apps/passwordcheck/MainActivity.java
@@ -1,25 +1,29 @@
 package edu.cascadia.mobile.apps.passwordcheck;
 
+import androidx.databinding.Bindable;
 import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.databinding.DataBindingUtil;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import edu.cascadia.mobile.apps.passwordcheck.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
     //TODO: create a private ViewModel instance variable
+    private PasswordViewModel mViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //Comment out the setContentView - we'll use a binding instead
-        setContentView(R.layout.activity_main);
 
+        ActivityMainBinding binding =
+                DataBindingUtil.setContentView(this, R.layout.activity_main);
         //TODO: Create a new binding using DataBindingUtil
-
+        mViewModel = new PasswordViewModel();
+        binding.setViewModel(mViewModel);
         //TODO: Create a new PasswordViewModel object
-
         //TODO: Set the binding's ViewModel to your new PasswordViewModel
 
 
