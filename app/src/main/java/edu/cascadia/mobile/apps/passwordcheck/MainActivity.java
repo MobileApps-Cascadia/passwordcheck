07174b801618cff09777b447300874b1057f7561
Charles Sy <charles.m.sy@hotmail.com>
2020-02-20 18:18:38 -0800

    Migration to androidx. todo is checked off and done.

--- app/src/main/java/edu/cascadia/mobile/apps/passwordcheck/MainActivity.java
+++ app/src/main/java/edu/cascadia/mobile/apps/passwordcheck/MainActivity.java
@@ -1,9 +1,9 @@
 package edu.cascadia.mobile.apps.passwordcheck;
 
-import android.arch.lifecycle.ViewModel;
-import android.arch.lifecycle.ViewModelProvider;
-import android.databinding.DataBindingUtil;
-import android.support.v7.app.AppCompatActivity;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.databinding.DataBindingUtil;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import edu.cascadia.mobile.apps.passwordcheck.databinding.ActivityMainBinding;
@@ -12,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
 
     //TODO: create a private ViewModel instance variable
 
+    private ViewModel mviewModel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,10 +21,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //TODO: Create a new binding using DataBindingUtil
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
         //TODO: Create a new PasswordViewModel object
 
+        PasswordViewModel pvm = new PasswordViewModel();
+
         //TODO: Set the binding's ViewModel to your new PasswordViewModel
+
+        binding.setViewModel(pvm);
 
 
     }
