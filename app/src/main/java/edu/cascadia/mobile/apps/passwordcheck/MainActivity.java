d9e1a46b82e8fdf6cdb496ba3487a2d17a512873
Teylan <grantsrockets@gmail.com>
2020-03-18 23:33:36 -0700

    Completed, it should work but I can't build the project because Android studio keeps telling me it can't resolve the generated ActivityMainBinding class. I made sure its named correctly in MainActivity but for some reason Android Studio still can't resolve it.

--- app/src/main/java/edu/cascadia/mobile/apps/passwordcheck/MainActivity.java
+++ app/src/main/java/edu/cascadia/mobile/apps/passwordcheck/MainActivity.java
@@ -4,17 +4,24 @@ import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.databinding.DataBindingUtil;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
 import android.os.Bundle;
+
+import edu.cascadia.mobile.apps.passwordcheck.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     //TODO: create a private ViewModel instance variable
-
+    PasswordViewModel mViewModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //Comment out the setContentView - we'll use a binding instead
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        mViewModel = new PasswordViewModel();
+        binding.setViewModel(mViewModel);
 
         //TODO: Create a new binding using DataBindingUtil
 
