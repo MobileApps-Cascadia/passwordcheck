24b28bd314764e0d1f3c107ca53e035349dfb586
Andrea Harrison <Andrealharrison@gmail.com>
2022-02-17 22:53:52 -0800

    updated activity_main.xml and main activity

--- app/src/main/java/edu/cascadia/mobile/apps/passwordcheck/PasswordViewModel.java
+++ app/src/main/java/edu/cascadia/mobile/apps/passwordcheck/PasswordViewModel.java
@@ -1,7 +1,7 @@
 package edu.cascadia.mobile.apps.passwordcheck;
 // Adapted from https://www.bignerdranch.com/blog/two-way-data-binding-on-android-observing-your-view-with-xml/
 import androidx.databinding.BaseObservable;
-import com.android.databinding.library.baseAdapters.BR;
+
 
 import androidx.databinding.Bindable;
 
@@ -27,7 +27,7 @@ public class PasswordViewModel extends BaseObservable {
     }
 
     public void setPassword(String password) {
-        if(this.password != password) {
+        if(!this.password.equals(password)) {
             this.password = password;
             notifyPropertyChanged(BR.passwordQuality);
             notifyPropertyChanged(BR.password);
